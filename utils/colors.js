9d4b39d90b893afa7bbae50fb28334d4abcb754b
Scriptonita <contacto@scriptonita.com>
2017-10-14 23:45:30 +0200

    Add Color Scheme

---
+++ utils/colors.js
@@ -0,0 +1,14 @@
+/** Color Scheme
+*
+* Webapp used to get color scheme:
+* https://coolors.co/
+*
+* Scheme choosen:
+* https://coolors.co/90d7ff-c9f9ff-4dccbd-bfd0e0-b8b3be
+**/
+
+export const blue = "#90d7ff";
+export const lightBlue = "#c9f9ff";
+export const green = "#4dccbd";
+export const gray = "#bfd0e0";
+export const redGray = "#b8b3be";
